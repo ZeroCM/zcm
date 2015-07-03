a2db59d7b317f14bfe6c1d7138739ab3e25a2932
Anthony J Bonkoski <ajbonkoski@gmail.com>
2015-07-03 15:31:38 -0400

    Added an emit/ subdir

--- c/zcm-gen/Main.cpp
+++ c/zcm-gen/Main.cpp
@@ -1,32 +1,12 @@
 #include "Common.hpp"
 #include "GetOpt.hpp"
 #include "ZCMGen.hpp"
+#include "emit/Emit.hpp"
 
 extern "C" {
 #include "version.h"
 #include "tokenize.h"
 }
-
-void setupOptionsC(GetOpt& gopt);
-int emitC(ZCMGen& zcm);
-
-/* void setup_java_options(getopt_t *gopt); */
-/* int emit_java(zcmgen_t *zcm); */
-
-/* void setup_python_options(getopt_t *gopt); */
-/* int emit_python(zcmgen_t *zcm); */
-
-/* void setup_js_options(getopt_t *gopt); */
-/* int emit_js(zcmgen_t *zcm); */
-
-/* void setup_lua_options(getopt_t *gopt); */
-/* int emit_lua(zcmgen_t *zcm); */
-
-/* void setup_csharp_options(getopt_t *gopt); */
-/* int emit_csharp(zcmgen_t *zcm); */
-
-/* void setup_cpp_options(getopt_t *gopt); */
-/* int emit_cpp(zcmgen_t *zcm); */
 
 int main(int argc, char *argv[])
 {
---
+++ c/zcm-gen/emit/Emit.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+void setupOptionsC(GetOpt& gopt);
+int emitC(ZCMGen& zcm);
+
+/* void setup_java_options(getopt_t *gopt); */
+/* int emit_java(zcmgen_t *zcm); */
+
+/* void setup_python_options(getopt_t *gopt); */
+/* int emit_python(zcmgen_t *zcm); */
+
+/* void setup_js_options(getopt_t *gopt); */
+/* int emit_js(zcmgen_t *zcm); */
+
+/* void setup_lua_options(getopt_t *gopt); */
+/* int emit_lua(zcmgen_t *zcm); */
+
+/* void setup_csharp_options(getopt_t *gopt); */
+/* int emit_csharp(zcmgen_t *zcm); */
+
+/* void setup_cpp_options(getopt_t *gopt); */
+/* int emit_cpp(zcmgen_t *zcm); */
