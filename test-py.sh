07624b6223ffa1e42852acf130a4434d0b17871c
Anthony J Bonkoski <ajbonkoski@gmail.com>
2015-07-10 17:25:50 -0400

    added a testing script for python

---
+++ test-py.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+THISDIR=$(pwd)
+
+## Note: this runs in a subshell
+gen_lcm() {(
+    file=$1
+    rm -fr /tmp/lcmtypes
+    mkdir /tmp/lcmtypes
+    cd /tmp/lcmtypes
+    lcm-gen -p $file
+    sed -i 's/lcm/zcm/g' /tmp/lcmtypes/*
+    sed -i 's/LCM/ZCM/g' /tmp/lcmtypes/*
+)}
+
+## Note: this runs in a subshell
+gen_zcm() {(
+    file=$1
+    rm -fr /tmp/zcmtypes
+    mkdir /tmp/zcmtypes
+    cd /tmp/zcmtypes
+    $THISDIR/build/zcm-gen/zcm-gen -p $file
+)}
+
+ZCMFILES=$(ls test/zcm-gen/*.zcm)
+
+for f in $ZCMFILES; do
+    name=$(basename $f)
+    bname=${name%.*}
+    gen_lcm $THISDIR/$f
+    gen_zcm $THISDIR/$f
+    diff /tmp/lcmtypes/$bname.py /tmp/zcmtypes/$bname.py
+done
