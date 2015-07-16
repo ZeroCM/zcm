f2509c02525977e7c315245355b1686b27fa1b65
Anthony J Bonkoski <ajbonkoski@gmail.com>
2015-07-15 22:44:50 -0400

    Example in Java is working now!

---
+++ examples/java/example/apps/Sub.java
@@ -0,0 +1,62 @@
+package example.apps;
+
+import java.io.*;
+import zcm.zcm.*;
+import example.zcmtypes.example_t;
+
+public class Sub implements ZCMSubscriber
+{
+    ZCM zcm;
+
+    public Sub()
+    {
+        zcm = ZCM.getSingleton();
+        zcm.subscribe("EXAMPLE", this);
+    }
+
+    public void messageReceived(ZCM zcm, String channel, ZCMDataInputStream ins)
+    {
+        System.out.println("Received message on channel " + channel);
+
+        try {
+            if (channel.equals("EXAMPLE")) {
+                example_t msg = new example_t(ins);
+
+                System.out.println("  timestamp    = " + msg.timestamp);
+                System.out.println("  position     = [ " + msg.position[0] +
+                                   ", " + msg.position[1] + ", " + msg.position[2] + " ]");
+                System.out.println("  orientation  = [ " + msg.orientation[0] +
+                                   ", " + msg.orientation[1] +
+                                   ", " + msg.orientation[2] +
+                                   ", " + msg.orientation[3] + " ]");
+
+                System.out.print("  ranges       = [ ");
+                for (int i=0; i<msg.num_ranges; i++) {
+                    System.out.print("" + msg.ranges[i]);
+                    if (i < msg.num_ranges-1)
+                        System.out.print (", ");
+                }
+                System.out.println (" ]");
+                System.out.println("  name         = '" + msg.name + "'");
+                System.out.println("  enabled      = '" + msg.enabled + "'");
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Exception: " + ex);
+        }
+    }
+
+    static void sleep(long ms)
+    {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {}
+    }
+
+    public static void main(String[] args)
+    {
+        Sub s = new Sub();
+        while(true)
+            sleep(1000);
+    }
+}
