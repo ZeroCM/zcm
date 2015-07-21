ef7a04b933457913cc39141e5ea75d551363356a
Anthony J Bonkoski <ajbonkoski@gmail.com>
2015-07-21 18:37:40 -0400

    simplified the client side script

--- examples/node-client/public/js/index.js
+++ examples/node-client/public/js/index.js
@@ -1,14 +1,7 @@
-var app = (function(){
-    function onload() {
-        var z = zcm.create()
-        z.subscribe('EXAMPLE', function(channel, msg) {
-            console.log('Got EXAMPLE: ', msg);
-            z.publish('FOOBAR', msg);
-        });
-    }
-    return {
-        onload: onload,
-    };
-})();
-
-onload = app.onload;
+onload = function(){
+    var z = zcm.create()
+    z.subscribe('EXAMPLE', function(channel, msg) {
+        console.log('Got EXAMPLE: ', msg);
+        z.publish('FOOBAR', msg);
+    });
+}
