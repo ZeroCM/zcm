4bf70397b7132cfcb544a98eecdc23c4b4af4e5a
Anthony J Bonkoski <ajbonkoski@gmail.com>
2015-08-03 20:33:11 -0400

    Rewrote the testing script to find all of the .c and .h files automatically. This is required for the testing of nested types

--- scripts/test-gen-c.sh
+++ scripts/test-gen-c.sh
@@ -18,27 +18,36 @@ gen_zcm() {(
     $THISDIR/build/gen/zcm-gen -c $file
 )}
 
-check() {
+check_all_ext() {
     bname=$1
     ext=$2
-    fname=$bname.$ext
 
-    cand=$TMPDIR/$fname
-    orig=$TESTDIR/$bname.ans/$fname
+    ## find all files with the requested extention in both this dir and
+    candfiles=$(find $TMPDIR | grep "\.$ext"'$')
+    origfiles=$(find $TESTDIR/$bname.ans | grep "\.$ext"'$')
+    collected=($candfiles $origfiles)
+    files=$(echo "${collected[@]}" | xargs -n 1 basename | sort -u)
 
-    diff $cand $orig > /tmp/zcm-test.out 2>&1
-    if [ "$?" != "0" ]; then
-        echo "Error: mismatch btwn $cand and $orig"
+    for f in $files; do
+        cand=$TMPDIR/$f
+        orig=$TESTDIR/$bname.ans/$f
         if [ "$VERBOSE" == "1" ]; then
-            cat /tmp/zcm-test.out
+            echo "CHECK: '$cand' VS '$orig'"
         fi
-    fi
+        diff $cand $orig > /tmp/zcm-test.out 2>&1
+        if [ "$?" != "0" ]; then
+            echo "Error: mismatch btwn $cand and $orig"
+            if [ "$VERBOSE" == "1" ]; then
+                cat /tmp/zcm-test.out
+            fi
+        fi
+    done
 }
 
 compare() {
     bname=$1
-    check $bname h
-    check $bname c
+    check_all_ext $bname h
+    check_all_ext $bname c
 }
 
 ZCMFILES=$(ls $TESTDIR/*.zcm)
@@ -46,6 +55,11 @@ ZCMFILES=$(ls $TESTDIR/*.zcm)
 for f in $ZCMFILES; do
     name=$(basename $f)
     bname=${name%.*}
+
+    if [ "$VERBOSE" == "1" ]; then
+        echo "TEST: $bname"
+    fi
+
     gen_zcm $THISDIR/$f
     compare $bname
 done
