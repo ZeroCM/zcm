f1c0f9599bc470f7fa2c9cbac28169c767859929
Anthony J Bonkoski <ajbonkoski@gmail.com>
2015-08-30 17:48:34 -0400

    added mutual exclusion to ZCM_DEBUG()

--- zcm/util/debug.cpp
+++ zcm/util/debug.cpp
@@ -3,6 +3,11 @@
 #ifdef ZCM_DEBUG_ENV_ENABLE
 bool ZCM_DEBUG_ENABLED;
 
+#include <mutex>
+static std::mutex mut;
+void zcm_debug_lock(void) { mut.lock(); }
+void zcm_debug_unlock(void) { mut.unlock(); }
+
 // This code runs before the main() function to load check if
 // the ZCM_DEBUG env-var is set
 struct BeforeMainCode
