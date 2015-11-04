d74f9637972dcb666b1b639dbb95cf823947851c
Isaac Olson <olsoni@umich.edu>
2015-11-04 17:16:30 -0500

    wip
    need to change how the generated types file works because it can't
    `require('ref')` inside of it without being directory specific

--- examples/node-client/app.js
+++ examples/node-client/app.js
@@ -4,10 +4,28 @@ var http = require('http').Server(app);
 app.use(express.static("public"));
 
 var zcm = require('zcm');
-var zcmtypes = require('./zcmtypes');
+var zcmtypes = require('../build/types/zcmtypes');
 var z = zcm.create(zcmtypes, null, http);
 if (!z) {
     throw "Failed to create ZCM";
+
+var sub1;
+var sub2;
+var sub3;
+
+function subTo1() {
+    sub1 = z.subscribe("EXAMPLE", "example_t", function(channel, msg) {
+        z.publish("FOOBAR_SERVER_1", "example_t", {
+            timestamp: 0,
+            position: [2, 4, 6],
+            orientation: [0, 2, 4, 6],
+            num_ranges: 2,
+            ranges: [7, 6],
+            name: 'foobar string',
+            enabled: false,
+        });
+    });
+>>>>>>> wip
 }
 
 setInterval(function() {
