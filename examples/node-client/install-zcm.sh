523b9c09d1e4969d8a2eb290df4853f8b8547f6d
Jonathan Bendes <jonathan@skyspecs.com>
2016-07-02 23:37:10 -0400

    Fixed small node bug

--- examples/node-client/install-zcm.sh
+++ examples/node-client/install-zcm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ "$1" == "local" ]; then
-    cp ../../zcm/js/node/index.js node_modules/zcm/.
+    mkdir -p node_modules/zcm
+    cp ../../zcm/js/node/index.js node_modules/zcm/
 else
     npm install ../../build/zcm/js/zcm-*.tgz
 fi
