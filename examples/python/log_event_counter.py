487c222183bd593fdd29d7f05098bd1b77e02dc0
Jonathan Bendes <jonathan@skyspecs.com>
2016-07-22 17:31:25 -0400

    Added log event counter

---
+++ examples/python/log_event_counter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from zcm import ZCM, LogFile, LogEvent
+import sys
+
+if len(sys.argv) < 2:
+    print "Usage: ./log_event_counter.py <logfile.log>"
+    exit(1)
+
+log = LogFile(sys.argv[1], 'r')
+
+if not log.good():
+    print "Unable to open log"
+    exit(1)
+
+i = 0
+evt = log.readNextEvent()
+while evt:
+    i = i + 1
+    evt = log.readNextEvent()
+log.close()
+
+print "Total events: " + str(i)
