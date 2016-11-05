48b3ace480baea71fd8ca7d53965f1b1ee19be89
Jonathan Bendes <jonathan@skyspecs.com>
2016-11-05 12:18:28 -0400

    Added zcm as an npm dependency. Can still install from local build

--- examples/node-client/install-zcm.sh
+++ examples/node-client/install-zcm.sh
@@ -2,8 +2,8 @@
 if [ "$1" == "local" ]; then
     mkdir -p node_modules/zcm
     cp ../../zcm/js/node/index.js node_modules/zcm/
-else
-    npm install ../../build/zcm/js/zcm-*.tgz
+#else
+    #npm install ../../build/zcm/js/zcm-*.tgz
 fi
 
 cp ../../build/zcm/js/zcm-client.js public/js/.
