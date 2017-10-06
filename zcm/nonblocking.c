aa74d875ae865fc9998a2c8d097763237fada785
Isaac Olson <isaac.olson@skyspecs.com>
2017-10-05 21:03:17 -0400

    well figured out ... that zcm nonblocking unsubscribe just doesnt work in any way

--- zcm/nonblocking.c
+++ zcm/nonblocking.c
@@ -5,7 +5,10 @@
 #include <string.h>
 
 /* TODO remove malloc for preallocated mem and linked-lists */
-#define ZCM_NONBLOCK_SUBS_MAX 16
+/* RRR: DEFINITELY make this noted somewhere */
+#ifndef ZCM_NONBLOCK_SUBS_MAX
+#define ZCM_NONBLOCK_SUBS_MAX 512
+#endif
 
 struct zcm_nonblocking
 {
@@ -15,8 +18,9 @@ struct zcm_nonblocking
     bool allChannelsEnabled;
 
     /* TODO speed this up */
-    size_t nsubs;
     zcm_sub_t subs[ZCM_NONBLOCK_SUBS_MAX];
+    bool      subInUse[ZCM_NONBLOCK_SUBS_MAX];
+    size_t    subInUseEnd;
 };
 
 static bool isRegexChannel(const char* c, size_t clen)
@@ -59,7 +63,7 @@ zcm_nonblocking_t *zcm_nonblocking_create(zcm_t *z, zcm_trans_t *zt)
     zcm->z = z;
     zcm->zt = zt;
     zcm->allChannelsEnabled = false;
-    zcm->nsubs = 0;
+    zcm->subInUseEnd = 0;
     return zcm;
 }
 
@@ -107,49 +111,47 @@ zcm_sub_t *zcm_nonblocking_subscribe(zcm_nonblocking_t *zcm, const char *channel
         return NULL;
     }
 
-    i = zcm->nsubs;
-    if (i >= ZCM_NONBLOCK_SUBS_MAX) {
-        return NULL;
-    }
-    strncpy(zcm->subs[i].channel, channel,
-            sizeof(zcm->subs[i].channel)/sizeof(zcm->subs[i].channel[0]));
-    zcm->subs[i].callback = cb;
-    zcm->subs[i].usr = usr;
-    zcm->nsubs++;
+    for (i = 0; i <= zcm->subInUseEnd && i < ZCM_NONBLOCK_SUBS_MAX; ++i) {
+        if (!zcm->subInUse[i]) {
 
-    return &zcm->subs[i];
+            strncpy(zcm->subs[i].channel, channel,
+                    sizeof(zcm->subs[i].channel)/sizeof(zcm->subs[i].channel[0]));
+            zcm->subs[i].callback = cb;
+            zcm->subs[i].usr = usr;
+            zcm->subInUse[i] = true;
+
+            if (i == zcm->subInUseEnd) ++zcm->subInUseEnd;
+
+            return &zcm->subs[i];
+        }
+    }
+    return NULL;
 }
 
 int zcm_nonblocking_unsubscribe(zcm_nonblocking_t *zcm, zcm_sub_t *sub)
 {
     size_t i;
-    size_t match_idx = zcm->nsubs;
+    int    match_idx = sub - zcm->subs;
     size_t num_chan_matches = 0;
     int rc = ZCM_EOK;
-    for (i = 0; i < zcm->nsubs; i++) {
+    for (i = 0; i < zcm->subInUseEnd; ++i) {
         /* Note: it would be nice if we didn't have to do a string comp to unsubscribe, but
                  we need to count the number of channel matches so we know when we can disable
-                 the transport's recvvmsg_enable */
+                 the transport's recvmsg_enable */
         if (strncmp(sub->channel, zcm->subs[i].channel,
                     sizeof(zcm->subs[i].channel)/sizeof(zcm->subs[i].channel[0])) == 0) {
             ++num_chan_matches;
-            if (sub == &zcm->subs[i]) {
-                match_idx = i;
-            }
         }
     }
 
-    if (match_idx != zcm->nsubs) {
+    if (0 <= match_idx && match_idx < zcm->subInUseEnd) {
         if (num_chan_matches <= 1) {
             rc = zcm_trans_recvmsg_enable(zcm->zt, sub->channel, false);
         }
 
-        --zcm->nsubs;
-        if (match_idx < zcm->nsubs) {
-            strncpy(zcm->subs[match_idx].channel, zcm->subs[zcm->nsubs].channel,
-                    sizeof(zcm->subs[i].channel)/sizeof(zcm->subs[i].channel[0]));
-            zcm->subs[match_idx].callback = zcm->subs[zcm->nsubs].callback;
-            zcm->subs[match_idx].usr = zcm->subs[zcm->nsubs].usr;
+        zcm->subInUse[match_idx] = false;
+        while (zcm->subInUseEnd > 0 && !zcm->subInUse[zcm->subInUseEnd - 1]) {
+            --zcm->subInUseEnd;
         }
     } else {
         rc = ZCM_EINVALID;
@@ -164,7 +166,9 @@ static void dispatch_message(zcm_nonblocking_t *zcm, zcm_msg_t *msg)
     zcm_sub_t *sub;
 
     size_t i;
-    for (i = 0; i < zcm->nsubs; i++) {
+    for (i = 0; i < zcm->subInUseEnd; ++i) {
+        if (!zcm->subInUse[i]) continue;
+
         size_t subsChanLen = strlen(zcm->subs[i].channel);
         if (isRegexChannel(zcm->subs[i].channel, subsChanLen)) {
             size_t msgLen = strlen(msg->channel);
