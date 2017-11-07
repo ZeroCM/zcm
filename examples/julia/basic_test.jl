98745522a576c9533576e5476789c337408a4d7c
Jonathan Bendes <jonathan.bendes@gmail.com>
2017-11-06 21:38:49 -0500

    Minor improvements to tests examples and emitters

--- examples/julia/basic_test.jl
+++ examples/julia/basic_test.jl
@@ -1,11 +1,14 @@
 include("/usr/local/share/julia/zcm.jl");
+include("../build/types/example_t.jl");
 
 import ZCM;
 
 numReceived = 0
-handler = function(rbuf::ZCM.RecvBuf, channel::String, usr)
+handler = function(rbuf::ZCM.RecvBuf, channel::String, msg::example_t, usr)
     println("Received message on channel: ", channel)
-    global numReceived = numReceived + 1
+    global numReceived
+    @assert (numReceived == msg.timestamp) "Received message with incorrect timestamp"
+    numReceived = numReceived + 1
 end
 
 zcm = ZCM.Zcm("inproc")
@@ -14,22 +17,30 @@ if (!zcm.good())
     exit()
 end
 
-sub = zcm.subscribeRaw("EXAMPLE", handler, Void)
+sub = zcm.subscribe("EXAMPLE", example_t, handler, Void)
+
+msg = example_t()
 
 zcm.start()
 
-zcm.publishRaw("EXAMPLE", [0x00], UInt32(length([0x00])))
-zcm.publishRaw("EXAMPLE", [0x00], UInt32(length([0x00])))
-zcm.publishRaw("EXAMPLE", [0x00], UInt32(length([0x00])))
+msg.timestamp = 0;
+zcm.publish("EXAMPLE", msg)
+msg.timestamp = 1;
+zcm.publish("EXAMPLE", msg)
+msg.timestamp = 2;
+zcm.publish("EXAMPLE", msg)
 
 sleep(0.5)
 zcm.stop()
 
 zcm.start()
 
-zcm.publishRaw("EXAMPLE", [0x00], UInt32(length([0x00])))
-zcm.publishRaw("EXAMPLE", [0x00], UInt32(length([0x00])))
-zcm.publishRaw("EXAMPLE", [0x00], UInt32(length([0x00])))
+msg.timestamp = 3;
+zcm.publish("EXAMPLE", msg)
+msg.timestamp = 4;
+zcm.publish("EXAMPLE", msg)
+msg.timestamp = 5;
+zcm.publish("EXAMPLE", msg)
 
 sleep(0.5)
 zcm.stop()
