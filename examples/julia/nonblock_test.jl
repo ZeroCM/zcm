714080afb35e436d43dd87d20640b65329c77d78
Jonathan Bendes <jonathan.bendes@gmail.com>
2017-11-07 12:02:51 -0500

    Encode decode not working

--- examples/julia/nonblock_test.jl
+++ examples/julia/nonblock_test.jl
@@ -1,7 +1,6 @@
 include("/usr/local/share/julia/zcm.jl");
 include("../build/types/example_t.jl");
 
-import ZCM;
 using ZCM;
 
 numReceived = 0
@@ -13,26 +12,26 @@ handler = function(rbuf::ZCM.RecvBuf, channel::String, msg::example_t, usr)
 end
 
 zcm = Zcm("nonblock-inproc")
-if (!zcm_good(zcm))
+if (!good(zcm))
     println("Unable to initialize zcm");
     exit()
 end
 
-sub = zcm_subscribe(zcm, "EXAMPLE", example_t, handler, Void)
+sub = subscribe(zcm, "EXAMPLE", example_t, handler, Void)
 
 msg = example_t()
 
 msg.timestamp = 0;
-zcm_publish(zcm, "EXAMPLE", msg)
-zcm_handle_nonblock(zcm)
+publish(zcm, "EXAMPLE", msg)
+handle_nonblock(zcm)
 msg.timestamp = 1;
-zcm_publish(zcm, "EXAMPLE", msg)
-zcm_handle_nonblock(zcm)
+publish(zcm, "EXAMPLE", msg)
+handle_nonblock(zcm)
 msg.timestamp = 2;
-zcm_publish(zcm, "EXAMPLE", msg)
-zcm_handle_nonblock(zcm)
+publish(zcm, "EXAMPLE", msg)
+handle_nonblock(zcm)
 
-zcm_unsubscribe(zcm, sub)
+unsubscribe(zcm, sub)
 
 @assert (numReceived == 3) "Didn't receive proper number of messages"
 println("Success!")
