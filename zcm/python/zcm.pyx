6ed33ef65cac6ba1459695e3a437ce13186c61fd
Ivan Deylid <ivanov.dale@gmail.com>
2019-02-24 15:30:41 +0300

    Fix my bug

--- zcm/python/zcm.pyx
+++ zcm/python/zcm.pyx
@@ -135,7 +135,7 @@ cdef class ZCM:
         cdef const uint8_t* data = _data
         return zcm_publish(self.zcm, channel.encode('utf-8'), data, len(_data) * sizeof(uint8_t))
     def publishRaw(self, str channel, uint8_t* msg, int32_t length):
-        return zcm_publish(self.zcm, str channel.encode('utf-8'), uint8_t* msg, int32_t length)
+        return zcm_publish(self.zcm, channel.encode('utf-8'), msg, length)
     def flush(self):
         while zcm_try_flush(self.zcm) != ZCM_EOK:
             time.sleep(0) # yield the gil
