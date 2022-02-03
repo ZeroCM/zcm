8984935a3ad899d357684d08d4eee8052dd1177e
Isaac Olson <isaac.olson@skyspecs.com>
2022-02-03 00:34:19 -0500

    done

--- tools/cpp/log-repair/main.cpp
+++ tools/cpp/log-repair/main.cpp
@@ -1,4 +1,13 @@
+#include <algorithm>
+#include <atomic>
 #include <iostream>
+#include <limits>
+#include <utility>
+#include <vector>
+
+#include <getopt.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include <zcm/zcm-cpp.hpp>
 
@@ -16,14 +25,12 @@ struct Args
 {
     string infile    = "";
     string outfile   = "";
-    bool   overwrite = false;
 
     bool init(int argc, char *argv[])
     {
         struct option long_opts[] = {
             { "help",                no_argument, 0, 'h' },
             { "output",        required_argument, 0, 'o' },
-            { "overwrite",           no_argument, 0, 'f' },
             { 0, 0, 0, 0 }
         };
 
@@ -31,7 +38,6 @@ struct Args
         while ((c = getopt_long(argc, argv, "ho:f", long_opts, 0)) >= 0) {
             switch (c) {
                 case 'o':          outfile   = string(optarg); break;
-                case 'f':          overwrite = true;           break;
                 case 'h': default: usage(); return false;
             };
         }
@@ -43,8 +49,6 @@ struct Args
         }
 
         infile = string(argv[optind]);
-
-        if (overwrite) outfile = infile;
 
         if (outfile.empty()) {
             cerr << "Must specify output file" << endl;
@@ -67,9 +71,108 @@ struct Args
              << "Options:" << endl
              << "" << endl
              << "  -o, --output=filename  specify output file" << endl
-             << "  -f, --overwrite        write output over the input file" << endl
              << "  -h, --help             Shows some help text and exits." << endl
              << endl;
+    }
+};
+
+struct LogRepair
+{
+    Args args;
+    zcm::LogFile *logIn  = nullptr;
+    zcm::LogFile *logOut = nullptr;
+
+    const zcm::LogEvent*         event;
+    off_t                        length;
+    off_t                        offset;
+    vector<pair<int64_t, off_t>> timestamps;
+    size_t                       progress;
+
+    LogRepair() { }
+
+    ~LogRepair()
+    {
+        if (logIn)  {
+            if (logIn->good()) logIn->close();
+            delete  logIn;
+        }
+        if (logOut) {
+            if (logOut->good()) logOut->close();
+            delete logOut;
+        }
+    }
+
+    bool init(int argc, char *argv[])
+    {
+        if (!args.init(argc, argv))
+            return false;
+
+        logIn = new zcm::LogFile(args.infile, "r");
+        if (!logIn->good()) {
+            cerr << "Error: Failed to open '" << args.infile << "'" << endl;
+            return false;
+        }
+
+        logOut = new zcm::LogFile(args.outfile, "w");
+        if (!logOut->good()) {
+            cerr << "Error: Failed to create output log" << endl;
+            return false;
+        }
+
+        fseeko(logIn->getFilePtr(), 0, SEEK_END);
+        length = ftello(logIn->getFilePtr());
+        fseeko(logIn->getFilePtr(), 0, SEEK_SET);
+
+        timestamps.reserve(1e6);
+
+        return true;
+    }
+
+    int run()
+    {
+        cout << "Reading log" << endl;
+        progress = 0;
+        cout << progress << "%" << flush;
+        while (true) {
+            if (done) return 1;
+
+            offset = ftello(logIn->getFilePtr());
+            event  = logIn->readNextEvent();
+            if (!event) break;
+
+            timestamps.emplace_back(event->timestamp, offset);
+
+            size_t p = (size_t)((100 * offset) / length);
+            if (p != progress) {
+                progress = p;
+                cout << "\r" << progress << "%" << flush;
+            }
+        }
+        cout << endl << "Read " << timestamps.size() << " events" << endl;
+
+        cout << "Repairing log data" << endl;
+        sort(timestamps.begin(), timestamps.end());
+
+        cout << "Writing new log" << endl;
+        progress = 0;
+        cout << progress << "%" << flush;
+        for (size_t i = 0; i < timestamps.size(); ++i) {
+            if (done) return 1;
+
+            size_t p = (100 * i) / timestamps.size();
+            if (p != progress) {
+                progress = p;
+                cout << "\r" << progress << "%" << flush;
+            }
+
+            logOut->writeEvent(logIn->readEventAtOffset(timestamps[i].second));
+        }
+
+        cout << endl << "Flushing to disk" << endl;
+        logIn->close();
+        logOut->close();
+
+        return 0;
     }
 };
 
@@ -85,7 +188,7 @@ int main(int argc, char* argv[])
 
     int ret = lr.run();
 
-    cout << "zcm-log-repair done" << endl;
+    cout << endl << "zcm-log-repair done" << endl;
 
     return ret;
 }
