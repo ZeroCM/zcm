0714b7c4586d4779d990ed1456671e2a58a298ed
Jonathan Bendes <jonathan@skyspecs.com>
2022-06-17 15:51:39 -0400

    Python to work with new versions of python

--- zcm/python/zcm-python.h
+++ zcm/python/zcm-python.h
@@ -2,7 +2,8 @@
 
 void PyEval_InitThreads_CUSTOM()
 {
-#if PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION == 9
+#if PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 7
+    Py_Initialize()
 #else
     PyEval_InitThreads();
 #endif
