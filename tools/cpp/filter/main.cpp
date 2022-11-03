bad56103208024ace99745f3b49ea81ee2ff4ec6
Jonathan Bendes <jonathan@skyspecs.com>
2022-11-03 00:49:00 -0400

    Implemented endpoint adjustments

--- tools/cpp/filter/main.cpp
+++ tools/cpp/filter/main.cpp
@@ -18,6 +18,7 @@ struct Args
     string inlog  = "";
     string outlog = "";
     bool debug    = false;
+    bool verbose  = false;
 
     unique_ptr<TypeDb> types;
 
@@ -277,7 +278,7 @@ struct Args
             // to be able to process more than one log in one call of this
             // program. No other tools in zcm have that functionality so
             // ignoring that for now.
-            static uint64_t firstUtime = std::numeric_limits<uint64_t>::max();
+            static uint64_t firstUtime = numeric_limits<uint64_t>::max();
             if (leTimestamp < firstUtime) firstUtime = leTimestamp;
 
             switch (type) {
@@ -440,6 +441,19 @@ struct Args
 
         vector<pair<string, regex>> channels;
 
+        int64_t beginAdjustment = 0;
+        int64_t endAdjustment = 0;
+
+        int64_t getBeginAdjustment() const
+        {
+            return beginAdjustment;
+        }
+
+        int64_t getEndAdjustment() const
+        {
+            return endAdjustment;
+        }
+
         bool isActive(const zcm::LogEvent* le) const
         {
             bool b = !begin || begin->isActive(le);
@@ -479,6 +493,8 @@ struct Args
             cout << idt << "Channels: ";
             if (channels.empty()) cout << "All";
             cout << endl;
+            cout << idt << "Begin Adjustment: " << (beginAdjustment / 1e6) << "s" << endl;
+            cout << idt << "End Adjustment: " << (endAdjustment / 1e6) << "s" << endl;
             for (auto& c : channels) cout << nidt << c.first << endl;
             cout << idt << "Begin: ";
             if (begin) {
@@ -636,6 +652,22 @@ struct Args
             return true;
         }
 
+        bool setBeginAdjustment(double beginAdjustmentSeconds)
+        {
+            if (regions.empty()) return false;
+            auto& back = regions.back();
+            back.beginAdjustment = beginAdjustmentSeconds * 1e6;
+            return true;
+        }
+
+        bool setEndAdjustment(double endAdjustmentSeconds)
+        {
+            if (regions.empty()) return false;
+            auto& back = regions.back();
+            back.endAdjustment = endAdjustmentSeconds * 1e6;
+            return true;
+        }
+
         void dump()
         {
             cout << "Regions: " << endl;
@@ -650,17 +682,21 @@ struct Args
     bool parse(int argc, char *argv[])
     {
         // set some defaults
-        const char *optstring = "i:o:t:dbec:arsf:n:l:g:h";
+        const char *optstring = "i:o:t:dvbec:arsf:n:l:g:h";
         struct option long_opts[] = {
             { "input",     required_argument, 0, 'i' },
             { "output",    required_argument, 0, 'o' },
             { "type-path", required_argument, 0, 't' },
             { "debug",     no_argument,       0, 'd' },
+            { "verbose",   no_argument,       0, 'v' },
             { "help",      no_argument,       0, 'h' },
 
             { "begin", no_argument, 0, 'b' },
             { "end",   no_argument, 0, 'e' },
-            { "channels", required_argument, 0, 'c' },
+
+            { "channels",     required_argument, 0, 'c' },
+            { "adjust-begin", required_argument, 0,  1  },
+            { "adjust-end",   required_argument, 0,  2  },
 
             { "and", no_argument, 0, 'a' },
             { "or",  no_argument, 0, 'r' },
@@ -685,6 +721,7 @@ struct Args
                     break;
                 }
                 case 'd': debug = true; break;
+                case 'v': verbose = true; break;
 
                 case 'b': {
                     if (!factory.addBegin()) {
@@ -703,6 +740,20 @@ struct Args
                 case 'c': {
                     if (!factory.setChannels(optarg)) {
                         cerr << "Improperly placed channels" << endl;
+                        return false;
+                    }
+                    break;
+                }
+                case 1: {
+                    if (!factory.setBeginAdjustment(atof(optarg))) {
+                        cerr << "Improperly placed begin adjustment" << endl;
+                        return false;
+                    }
+                    break;
+                }
+                case 2: {
+                    if (!factory.setEndAdjustment(atof(optarg))) {
+                        cerr << "Improperly placed end adjustment" << endl;
                         return false;
                     }
                     break;
@@ -829,6 +880,12 @@ struct Args
              << "    -e, --end                    Interpret the next args as defining end conditions" << endl
              << "    -c, --channels               Channels to keep when inside region" << endl
              << "                                 Provide as a comma separated list. Regex supported" << endl
+             << "        --adjust-begin           Number of seconds to adjust the beginning of each" << endl
+             << "                                 region by. Negative numbers indicate beginning" << endl
+             << "                                 should be moved to an earlier time" << endl
+             << "        --adjust-end             Number of seconds to adjust the end of each" << endl
+             << "                                 region by. Negative numbers indicate end" << endl
+             << "                                 should be moved to an earlier time" << endl
              << endl
              << "    -s, --seconds                Condition is based on seconds since beginning of zcm log" << endl
              << "    -f, --field=<field>          A config-style description of a flag in a channel/message" << endl
@@ -881,7 +938,6 @@ int main(int argc, char* argv[])
     }
     fseeko(inlog.getFilePtr(), 0, SEEK_END);
     off64_t logSize = ftello(inlog.getFilePtr());
-    fseeko(inlog.getFilePtr(), 0, SEEK_SET);
 
     zcm::LogFile outlog(args.outlog, "w");
     if (!outlog.good()) {
@@ -891,30 +947,82 @@ int main(int argc, char* argv[])
 
     if (args.debug) return 0;
 
-    size_t numInEvents = 0, numOutEvents = 0;
-    const zcm::LogEvent* evt;
-    off64_t offset;
+    auto processLog = [&](function<void(const zcm::LogEvent* evt)> processEvent){
+        const zcm::LogEvent* evt;
+        off64_t offset;
+        static int lastPrintPercent = 0;
 
-    static int lastPrintPercent = 0;
-    while (1) {
-        offset = ftello(inlog.getFilePtr());
+        fseeko(inlog.getFilePtr(), 0, SEEK_SET);
 
-        int percent = 100.0 * offset / (logSize == 0 ? 1 : logSize);
-        if (percent != lastPrintPercent) {
-            cout << "\r" << "Percent Complete: " << percent << flush;
-            lastPrintPercent = percent;
-        }
+        while (1) {
+            offset = ftello(inlog.getFilePtr());
 
-        evt = inlog.readNextEvent();
-        if (evt == nullptr) break;
-
-        bool keepEvent = false;
-        for (const auto& r : args.factory.regions) {
-            if (r.isActive(evt)) {
-                bool rKeep = r.keepEvent(evt->channel);
-                keepEvent |= rKeep;
-                // Intentionally not breaking here so every condition can see every event
+            int percent = 100.0 * offset / (logSize == 0 ? 1 : logSize);
+            if (percent != lastPrintPercent) {
+                cout << "\r" << "Percent Complete: " << percent << flush;
+                lastPrintPercent = percent;
             }
+
+            evt = inlog.readNextEvent();
+            if (evt == nullptr) break;
+
+            processEvent(evt);
+        }
+        if (lastPrintPercent != 100) cout << "\r" << "Percent Complete: 100" << flush;
+        cout << endl;
+    };
+
+    static constexpr int64_t i64max = numeric_limits<int64_t>::max();
+
+    cout << "Marking regions on first pass..." << endl;
+    vector<vector<pair<int64_t, int64_t>>> regions;
+    regions.resize(args.factory.regions.size());
+    processLog([&](const zcm::LogEvent* evt){
+        for (size_t i = 0; i < regions.size(); ++i) {
+            auto& r = regions[i];
+            bool active = args.factory.regions[i].isActive(evt);
+            if (!active) {
+                if (!r.empty() && r.back().second != i64max)
+                    r.emplace_back(i64max, i64max);
+            } else {
+                if (r.empty()) r.emplace_back(i64max, i64max);
+                if (r.back().first == i64max) r.back().first = evt->timestamp;
+                r.back().second = evt->timestamp;
+            }
+        }
+    });
+
+    size_t numRegions = 0;
+    for (size_t i = 0; i < regions.size(); ++i) {
+        if (!regions[i].empty() && regions[i].back().first == i64max) regions[i].pop_back();
+        numRegions += regions[i].size();
+        if (args.debug) cout << "Region " << i << ":" << endl;
+        for (size_t j = 0; j < regions[i].size(); ++j) {
+            auto& r = regions[i][j];
+            if (args.verbose) cout << "\t[" << r.first << ", " << r.second << "] -> ";
+            r.first += args.factory.regions[i].getBeginAdjustment();
+            r.second += args.factory.regions[i].getEndAdjustment();
+            if (args.verbose) cout << "[" << r.first << ", " << r.second << "]" << endl;
+        }
+    }
+    cout << "Adjusted endpoints of " << numRegions
+         << " region" << (numRegions > 1 ? "s" : "") << "..." << endl;
+
+    cout << "Writing events to output log..." << endl;
+    size_t numInEvents = 0, numOutEvents = 0;
+    processLog([&](const zcm::LogEvent* evt){
+        bool keepEvent = false;
+        for (size_t i = 0; i < regions.size(); ++i) {
+            auto& fr = args.factory.regions[i];
+            for (size_t j = 0; j < regions[i].size(); ++j) {
+                const auto& r = regions[i][j];
+                if (r.first <= evt->timestamp && evt->timestamp < r.second &&
+                    fr.keepEvent(evt->channel)) {
+                    keepEvent = true;
+                    break;
+                }
+            }
+            if (keepEvent) break;
         }
 
         if (keepEvent) {
@@ -923,9 +1031,7 @@ int main(int argc, char* argv[])
         }
 
         numInEvents++;
-    }
-    if (lastPrintPercent != 100) cout << "\r" << "Percent Complete: 100" << flush;
-    cout << endl;
+    });
 
     inlog.close();
     outlog.close();
