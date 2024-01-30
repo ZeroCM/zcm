b4a7b581442d5ca72809adbe368df7fbe984427a
xorvoid <xorvoid@gmail.com>
2024-01-30 13:30:38 -0600

    Prepare for merge

--- zcm/zcm.c
+++ zcm/zcm.c
@@ -40,14 +40,6 @@ zcm_t* zcm_create(const char* url)
     zcm_t* z = NULL;
     char *errmsg = NULL;
     int ret = zcm_try_create(&z, url, &errmsg);
-    // RRR (Bendes): this repo uses 4 space indents
-    // RRR (xorvoid): fixed this file: hunting for any others I missed. This is your chance to mock my Emacs usage haha
-    // RRR (Bendes): You can't print in zcm. printf isn't supported everywhere
-    //               Use ZCM_DEBUG
-    // RRR (xorvoid): Is it okay to use #ifndef ZCM_EMBEDDED ?? I consider this an important user diagnostic (more detailed explanation in slack)
-    //                Actually.. on further consideration: this entire block is ZCM_EMBEDDED becuase we manually create transports in embedded-land
-    //                I thought "no printf" only applied because of embedded? Am I misremembering or did policy change?
-    // RRR (xorvoid): Changed per slack chat
     ZCM_ASSERT(ret == ZCM_EOK);
     ZCM_ASSERT(z);
     return z;
