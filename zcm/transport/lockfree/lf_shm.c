f97cd7d11f712c6a8d83d2ed2cb735e55b7fcb9f
xorvoid <xorvoid@gmail.com>
2024-02-14 12:38:55 -0600

    ipcshm: add directory structure /dev/shm/zcm/ipcshm/<name> and use "default" when the name is unspecified

--- zcm/transport/lockfree/lf_shm.c
+++ zcm/transport/lockfree/lf_shm.c
@@ -6,43 +6,64 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
+#include <libgen.h>
 
-
-bool lf_shm_create(const char *name, size_t size)
+// like mkdir -p but skips the trailing component
+static void mkdir_with_parents(const char *path, mode_t mode)
 {
-  int shm_fd = shm_open(name, O_CREAT|O_RDWR|O_EXCL, 0644);
-  if (shm_fd < 0) return false;
-  if (ftruncate(shm_fd, size) < 0) {
+    int len = strlen(path);
+    for (int i = 0; i < len; i++) {
+        if (path[i]=='/') {
+            char *dirpath = (char *) malloc(i+1);
+            strncpy(dirpath, path, i);
+            dirpath[i]=0;
+
+            mkdir(dirpath, mode);
+            free(dirpath);
+
+            i++; // skip the '/'
+        }
+    }
+}
+
+bool lf_shm_create(const char *path, size_t size)
+{
+    mkdir_with_parents(path, 0775);
+
+    int shm_fd = open(path, O_CREAT|O_EXCL|O_RDWR|O_CLOEXEC, 0644);
+    if (shm_fd < 0) return false;
+
+    if (ftruncate(shm_fd, size) < 0) {
+        close(shm_fd);
+        return false;
+    }
     close(shm_fd);
-    return false;
-  }
-  close(shm_fd);
-  return true;
+    return true;
 }
 
-void lf_shm_remove(const char *name)
+void lf_shm_remove(const char *path)
 {
-  shm_unlink(name);
+    unlink(path);
 }
 
-void * lf_shm_open(const char *name, size_t * _opt_size)
+void * lf_shm_open(const char *path, size_t * _opt_size)
 {
-  int shm_fd = shm_open(name, O_CREAT|O_RDWR, 0755);
-  if (shm_fd < 0) return NULL;
+    int shm_fd = open(path, O_RDWR|O_CLOEXEC);
+    if (shm_fd < 0) return NULL;
 
-  struct stat st[1];
-  if (0 != fstat(shm_fd, st)) return NULL;
-  size_t size = st->st_size;
+    struct stat st[1];
+    if (0 != fstat(shm_fd, st)) return NULL;
+    size_t size = st->st_size;
 
-  void *mem = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, 0);
-  if (mem == MAP_FAILED) return NULL;
+    void *mem = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (mem == MAP_FAILED) return NULL;
 
-  close(shm_fd);
-  if (_opt_size) *_opt_size = size;
-  return mem;
+    close(shm_fd);
+    if (_opt_size) *_opt_size = size;
+    return mem;
 }
 
 void lf_shm_close(void *shm, size_t size)
 {
-  munmap(shm, size);
+    munmap(shm, size);
 }
