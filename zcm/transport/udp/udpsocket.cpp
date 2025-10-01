ae4437c9b1c77118175ceca390fe801d2eed8bb7
Jonathan Bendes <jonathan@skyspecs.com>
2025-10-01 15:38:08 -0400

    Fixing call to ambiguous function in udp socket

--- zcm/transport/udp/udpsocket.cpp
+++ zcm/transport/udp/udpsocket.cpp
@@ -133,7 +133,7 @@ bool UDPSocket::bindPort(u16 port)
     addr.sin_addr.s_addr = INADDR_ANY;
     addr.sin_port = htons(port);
 
-    if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    if (::bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
         perror("bind");
         return false;
     }
