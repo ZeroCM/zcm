0c29e025f928a9c267e648f6213f3ada53070a1c
Jonathan Bendes <jonathan@skyspecs.com>
2025-10-22 13:27:15 -0400

    Added configurable use of montonic clock source

--- util/TimeUtil.hpp
+++ util/TimeUtil.hpp
@@ -6,8 +6,14 @@ namespace TimeUtil
 {
     static u64 utime()
     {
+#ifdef MONOTONIC_CLOCK
+        struct timespec ts;
+        clock_gettime(CLOCK_MONOTONIC_RAW, &ts); // or CLOCK_MONOTONIC
+        return (uint64_t)ts.tv_sec * 1000000ULL + ts.tv_nsec / 1000ULL;
+#else
         struct timeval tv;
         gettimeofday(&tv, NULL);
         return (u64)tv.tv_sec * 1000000 + tv.tv_usec;
+#endif
     }
 }
